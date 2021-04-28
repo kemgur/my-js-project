9e63c99b57cf8fca2dbc226ffbd03ef10c4ce250
techproed <abayram.techproed@gmail.com>
2021-04-27 19:11:46 -0500

    committing day01 and day02

---
+++ Day01_Variables.js
@@ -0,0 +1,33 @@
+// VARIABLES:
+console.clear();
+// Declaring name variable with no initial value
+var name;
+console.log(name);//undefined
+
+//Declaring name variable with initial value
+var name = 'Ahmet';
+console.log(name);//Ahmet
+
+var name="Javascript";
+console.log(name);//Javascript
+
+name="Learning var keyword";
+console.log(name);
+
+// Declaring multiple variables
+// var fName;
+// var lName;
+
+// var fName, lName;
+
+var fName='John', lName='Cash';
+console.log(fName);//John
+console.log(lName);//Cash
+
+// Most common way to declare and initialize variables
+var firstName='Sam';
+var lastName='Walter';
+console.log(firstName);//Sam
+console.log(lastName);//Walter
+console.log(firstName+' '+lastName);//Sam Walter
+
